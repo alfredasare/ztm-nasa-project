b72c724a497a48b96b5bd97b929d1211241d353b
alfredasare <alfredasare101@gmail.com>
2021-07-03 22:37:11 +0000

    Load planets for tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,14 +1,16 @@
 const request = require('supertest');
 const app = require('../../app');
 const {mongoConnect, mongoDisconnect} = require('../../services/mongo');
+const {loadPlanetsData} = require('../../models/launches.model');
 
 describe('Launches API', () => {
     beforeAll(async() => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
-        await mongoConnect();
+        await mongoDisconnect();
     });
 
     describe('Test GET /launches', () => {
